9c61157ef54d15b5c8d8de47605dfaa700333b63
NVH <nvhoan911@gmail.com>
2022-02-28 15:46:29 +0700

    update menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuBox from "./MenuBox";
+import SpecialMenu from "./SpecialMenu";
+
+const Menu = () => {
+    return (
+        <>
+            <SpecialMenu />
+            <MenuBox/>
+        </>
+    );
+};
+
+export default Menu;
---
+++ src/components/Menu/SpecialMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SpecialMenu = () => {
+    return (
+        <>
+            <div class="all-page-title page-breadcrumb">
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>Special Menu</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SpecialMenu;
